05768253b8d0cb07c9df30686e65a8b4877f5242
Top Nguyen <admin@topnguyen.com>
2020-06-28 22:44:59 +0700

    update api

--- Goblin.Core/Models/GoblinApiRequestModel.cs
+++ Goblin.Core/Models/GoblinApiRequestModel.cs
@@ -3,11 +3,6 @@ namespace Goblin.Core.Models
     public class GoblinApiRequestModel
     {
         /// <summary>
-        ///     Authorization Key
-        /// </summary>
-        public string AuthorizationKey { get; set; }
-        
-        /// <summary>
         ///     UserId Key
         /// </summary>
         public long LoggedInUserId { get; set; }
