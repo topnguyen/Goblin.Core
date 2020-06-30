fac597bcee9ea33c6b131806209fe85e7dca5a4f
Top Nguyen <admin@topnguyen.com>
2020-06-30 20:33:11 +0700

    add jaeger as exception and validation log

--- Goblin.Core.Web/Filters/Exception/GoblinApiExceptionFilterAttribute.cs
+++ Goblin.Core.Web/Filters/Exception/GoblinApiExceptionFilterAttribute.cs
@@ -1,16 +1,28 @@
 ﻿using System;
-using Elect.Core.ObjUtils;
+using System.Linq;
+using System.Text.Json;
+using Elect.Core.EnvUtils;
 using Elect.Core.XmlUtils;
 using Elect.DI.Attributes;
 using Elect.Web.Models;
+using Goblin.Core.Errors;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using OpenTracing;
 
 namespace Goblin.Core.Web.Filters.Exception
 {
     [ScopedDependency]
     public class GoblinApiExceptionFilterAttribute : ExceptionFilterAttribute
     {
+        private readonly ITracer _jaegerTracer;
+
+        public GoblinApiExceptionFilterAttribute(ITracer jaegerTracer)
+        {
+            _jaegerTracer = jaegerTracer;
+        }
+
         public override void OnException(ExceptionContext context)
         {
             // Ignore cancel action
@@ -21,10 +33,20 @@ namespace Goblin.Core.Web.Filters.Exception
                 return;
             }
 
-            var errorModel = GoblinExceptionContextHelper.GetErrorModel(context);
+            var errorModel = GetErrorModel(context);
+
+            // Log to Jaeger
+            
+            LogToJaeger(context, errorModel, _jaegerTracer);
 
             // Response Result
 
+            if(!EnvHelper.IsDevelopment())
+            {
+                // Additional Data for Dev only
+                errorModel.AdditionalData = null;
+            }
+            
             if (context.HttpContext.Request.Headers[HeaderKey.Accept] == ContentType.Xml ||
                 context.HttpContext.Request.Headers[HeaderKey.ContentType] == ContentType.Xml)
                 context.Result = new ContentResult
@@ -34,18 +56,100 @@ namespace Goblin.Core.Web.Filters.Exception
                     StatusCode = context.HttpContext.Response.StatusCode
                 };
             else
+            {
                 context.Result = new ContentResult
                 {
                     ContentType = ContentType.Json,
-                    Content = errorModel.ToJsonString(),
+                    Content = JsonSerializer.Serialize(errorModel),
                     StatusCode = errorModel.StatusCode
                 };
+            }
 
             context.ExceptionHandled = true;
 
             // Keep base Exception
 
             base.OnException(context);
+        }
+
+        private static GoblinErrorModel GetErrorModel(ExceptionContext exceptionContext)
+        {
+            GoblinErrorModel errorModel;
+
+            switch (exceptionContext.Exception)
+            {
+                case GoblinException exception:
+                {
+                    errorModel = new GoblinErrorModel(exception);
+
+                    break;
+                }
+
+                case UnauthorizedAccessException _:
+                {
+                    errorModel = new GoblinErrorModel(nameof(GoblinErrorCode.UnAuthorized),
+                        GoblinErrorCode.UnAuthorized, StatusCodes.Status401Unauthorized);
+
+                    break;
+                }
+
+                default:
+                {
+                    var message = EnvHelper.IsDevelopment()
+                        ? exceptionContext.Exception.Message
+                        : GoblinErrorCode.Unknown;
+
+                    errorModel = new GoblinErrorModel(nameof(GoblinErrorCode.Unknown), message,
+                        StatusCodes.Status500InternalServerError);
+
+                    break;
+                }
+            }
+
+            errorModel.AdditionalData.Add("exception", new
+            {
+                message = exceptionContext.Exception.Message,
+                source = exceptionContext.Exception.Source,
+                stackTrade = exceptionContext.Exception.StackTrace,
+                innerException = new
+                {
+                    message = exceptionContext.Exception.InnerException?.Message,
+                    source = exceptionContext.Exception.InnerException?.Source,
+                    stackTrade = exceptionContext.Exception.InnerException?.StackTrace,
+                }
+            });
+
+            exceptionContext.HttpContext.Response.StatusCode = errorModel.StatusCode;
+
+            return errorModel;
+        }
+
+        private static void LogToJaeger(ExceptionContext exceptionContext, GoblinErrorModel errorModel, ITracer jaegerTracer)
+        {
+            var operationName = "💩😭💩 EXCEPTION 🔥🐛🔥";
+
+            var builder = jaegerTracer.BuildSpan(operationName);
+
+            using var scope = builder.StartActive(true);
+
+            var span = scope.Span;
+
+            if (errorModel != null)
+            {
+                jaegerTracer.ActiveSpan.SetTag("Exception Id", errorModel.Id);
+                jaegerTracer.ActiveSpan.SetTag("Exception Code", errorModel.Code);
+                jaegerTracer.ActiveSpan.SetTag("Exception Message", errorModel.Message);
+                jaegerTracer.ActiveSpan.SetTag("Exception Status Code", errorModel.StatusCode);
+
+                if (errorModel.AdditionalData?.Any() == true)
+                {
+                    span.Log(errorModel.AdditionalData);
+                }
+            }
+            else
+            {
+                span.Log(exceptionContext.Exception.Message);
+            }
         }
     }
 }
--- Goblin.Core.Web/Filters/Validation/GoblinApiValidationActionFilterAttribute.cs
+++ Goblin.Core.Web/Filters/Validation/GoblinApiValidationActionFilterAttribute.cs
@@ -5,31 +5,42 @@ using Elect.Core.XmlUtils;
 using Elect.DI.Attributes;
 using Elect.Web.Models;
 using Goblin.Core.Errors;
-using Goblin.Core.Settings;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using Newtonsoft.Json;
+using OpenTracing;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace Goblin.Core.Web.Filters.Validation
 {
     [ScopedDependency]
     public class GoblinApiValidationActionFilterAttribute : ActionFilterAttribute
     {
+        private readonly ITracer _jaegerTracer;
+
+        public GoblinApiValidationActionFilterAttribute(ITracer jaegerTracer)
+        {
+            _jaegerTracer = jaegerTracer;
+        }
+        
         public override void OnActionExecuting(ActionExecutingContext context)
         {
             if (context.ModelState.IsValid) return;
 
-            // Log Error
             var keyValueInvalidDictionary = GetModelStateInvalidInfo(context);
 
-            // Response Result
-            var apiErrorViewModel =
+            var errorModel =
                 new GoblinErrorModel(nameof(GoblinErrorCode.BadRequest), GoblinErrorCode.BadRequest,
                     StatusCodes.Status400BadRequest)
                 {
                     AdditionalData = keyValueInvalidDictionary
                 };
+            
+            // Log to Jaeger
+            
+            LogToJaeger(errorModel, _jaegerTracer);
+
+            // Response Result
 
             context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
 
@@ -38,7 +49,7 @@ namespace Goblin.Core.Web.Filters.Validation
                 {
                     ContentType = ContentType.Xml,
                     StatusCode = context.HttpContext.Response.StatusCode,
-                    Content = XmlHelper.ToXmlString(apiErrorViewModel)
+                    Content = XmlHelper.ToXmlString(errorModel)
                 };
             else
             {
@@ -46,12 +57,12 @@ namespace Goblin.Core.Web.Filters.Validation
                 {
                     ContentType = ContentType.Json,
                     StatusCode = context.HttpContext.Response.StatusCode,
-                    Content = JsonConvert.SerializeObject(apiErrorViewModel, GoblinJsonSetting.JsonSerializerSettings)
+                    Content = JsonSerializer.Serialize(errorModel)
                 };
             }
         }
 
-        public static Dictionary<string, object> GetModelStateInvalidInfo(ActionExecutingContext context)
+        private static Dictionary<string, object> GetModelStateInvalidInfo(ActionContext context)
         {
             var keyValueInvalidDictionary = new Dictionary<string, object>();
 
@@ -63,6 +74,30 @@ namespace Goblin.Core.Web.Filters.Validation
             }
 
             return keyValueInvalidDictionary;
+        }
+        
+        private static void LogToJaeger(GoblinErrorModel errorModel, ITracer jaegerTracer)
+        {
+            var operationName = "👀 INVALID DATA 🤣";
+
+            var builder = jaegerTracer.BuildSpan(operationName);
+
+            using var scope = builder.StartActive(true);
+
+            var span = scope.Span;
+
+            if (errorModel != null)
+            {
+                jaegerTracer.ActiveSpan.SetTag("Exception Id", errorModel.Id);
+                jaegerTracer.ActiveSpan.SetTag("Exception Code", errorModel.Code);
+                jaegerTracer.ActiveSpan.SetTag("Exception Message", errorModel.Message);
+                jaegerTracer.ActiveSpan.SetTag("Exception Status Code", errorModel.StatusCode);
+
+                if (errorModel.AdditionalData?.Any() == true)
+                {
+                    span.Log(errorModel.AdditionalData);
+                }
+            }
         }
     }
 }
